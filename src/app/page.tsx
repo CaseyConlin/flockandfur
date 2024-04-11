628939ab1bdfe75d9a0ee09919f366ea2a610a68
Casey Conlin <101153261+CaseyConlin@users.noreply.github.com>
2024-04-11 11:36:36 -0400

    Test deploy.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -71,7 +71,7 @@ export default function Home() {
           rel="noopener noreferrer"
         >
           <h2>
-            Templates <span>-&gt;</span>
+            Fried Templates <span>-&gt;</span>
           </h2>
           <p>Explore starter templates for Next.js.</p>
         </a>
